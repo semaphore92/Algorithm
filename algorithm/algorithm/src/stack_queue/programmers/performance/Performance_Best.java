324285cbde75ba0d0a741b187e8f1374949329ac
semaphore92 <swheo92@gmail.com>
2020-03-31 00:24:32 +0900

    프로그래머스 기능개발COMMIT

---
+++ algorithm/algorithm/src/stack_queue/programmers/performance/Performance_Best.java
@@ -0,0 +1,17 @@
+package stack_queue.programmers.performance;
+
+import java.util.Arrays;
+
+public class Performance_Best {
+	public int[] solution(int[] progresses, int[] speeds) {
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
+            }
+            dayOfend[day]++;
+        }
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
+    }
+}
